e9aa46eaacf9e0e8c2d7c27580cf4fe5c7b529cd
han <han@genisev.com>
2021-07-18 14:33:22 +0900

    Add Sample MsTest

---
+++ JenkinsMSTestSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinsMSTestSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinsMSTestSample")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad47bf55-9efc-4d9f-9904-0b235602b1e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JenkinsMSTestSample/SampleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsMSTestSample
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1;
+            Assert.AreEqual(1, a);
+        }
+    }
+}
